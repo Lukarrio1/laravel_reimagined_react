770b6fe5c51a8254e81b5fcdfe9139f51a2397dc
Jason <40766583+Lukarrio1@users.noreply.github.com>
2024-05-24 17:46:12 -0500

    Update PermissionWrapper.jsx

--- src/Laravel _Reimagined_Library/PermissionWrapper.jsx
+++ src/Laravel _Reimagined_Library/PermissionWrapper.jsx
@@ -1,18 +1,18 @@
 import { useSelector } from "react-redux";
 
 const PermissionWrapper = ({ uuid, children }) => {
-  const { componentOrLink } = useSelector((state) => {
+  const { hasAccess } = useSelector((state) => {
     const temp = [...state?.authentication.permissions.map((p) => p.id), 0];
     const currentNode = [
       ...state?.coreNodes.links,
       ...state?.coreNodes.components,
     ]?.filter((cl) => cl.uuid == uuid)[0]?.permission_id;
     return {
-      componentOrLink: temp.includes(currentNode),
+      hasAccess: temp.includes(currentNode),
     };
   });
 
-  return componentOrLink ? children : <></>;
+  return hasAccess ? children : <></>;
 };
 
 export default PermissionWrapper;
