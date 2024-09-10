c0593fc41054c198116a4aa81681dc05bbc80c61
lukarrio1 <tomennis1997@gmail.com>
2024-09-10 17:59:53 -0500

    updated the core of the app

--- src/Laravel _Reimagined_Library/Wrappers/AnimationWrapper.jsx
+++ src/Laravel _Reimagined_Library/Wrappers/AnimationWrapper.jsx
@@ -3,6 +3,6 @@ import { useSelector } from "react-redux";
 import useSettings from "../Custom Hooks/useSettings";
 
 export default function AnimationWrapper({ children }) {
-  const {getSetting} = useSettings()
-  return <div className={getSetting('app_animation')}>{children}</div>;
+  const { getSetting } = useSettings();
+  return <div className={getSetting("app_animation")}>{children}</div>;
 }
